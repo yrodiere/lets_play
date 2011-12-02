4d9316ba1efda4a7a07979c57843baacd62244ec
fenrhil <fenrhil@gmail.com>
2011-12-02 13:56:40 +0000

    Implemented icon factories.

--- src/ui/IconFactory.java
+++ src/ui/IconFactory.java
@@ -1,15 +1,31 @@
 package ui;
 
+import java.net.URL;
+import java.util.MissingResourceException;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 import data.Piece;
 import data.Tile;
 
 public abstract class IconFactory {
 	/* TODO: change return types to Swing icon */
-	public final Object createPieceIcon(Piece model) {
+	public final Icon createPieceIcon(Piece model) {
 		return createPieceIcon(model, model.getTypeHashCode());
 	}
 
-	public abstract Object createPieceIcon(Piece model, int newTypeHashCode);
+	public abstract Icon createPieceIcon(Piece model, int newTypeHashCode);
 
-	public abstract Object createTileIcon(Tile model);
+	public abstract Icon createTileIcon(Tile model);
+
+	protected final Icon createImageIcon(String path) {
+		URL url = getClass().getResource(path);
+		if (url != null) {
+			return new ImageIcon(url);
+		} else {
+			throw new MissingResourceException("Could not find an image icon",
+					getClass().getName(), path);
+		}
+	}
 }
