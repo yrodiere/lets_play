45215148a9bf4391f30d8f3eb84b5279a26fb1fc
fenrhil <fenrhil@gmail.com>
2011-12-02 12:20:46 +0000

    Finished implementing the data package (except Player).

--- src/data/Board.java
+++ src/data/Board.java
@@ -1,23 +1,47 @@
 package data;
 
-//TODO
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Observer;
+
 public class Board implements GameData {
+	private final Map<Coordinates, Tile> tiles = new HashMap<Coordinates, Tile>();
+
+	public Board(int rowCount, int columnCount) {
+		if (rowCount <= 0 || columnCount <= 0) {
+			throw new IllegalArgumentException(
+					"rowCount and columnCount must be strictly positive");
+		}
+
+		for (int i = 0; i < rowCount; ++i) {
+			for (int j = 0; j < columnCount; ++j) {
+				Coordinates coord = new Coordinates(i, j);
+				tiles.put(coord, new Tile(coord));
+			}
+		}
+	}
 
 	@Override
 	public void resetTurn() {
-		// TODO Auto-generated method stub
-		
+		for (Map.Entry<Coordinates, Tile> entry : tiles.entrySet()) {
+			entry.getValue().resetTurn();
+		}
 	}
 
 	@Override
 	public void endTurn() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public Tile findTileAt(Coordinates coords){
-		// TODO
-		return null;
+		for (Map.Entry<Coordinates, Tile> entry : tiles.entrySet()) {
+			entry.getValue().endTurn();
+		}
 	}
 
+	public Tile findTileAt(Coordinates coords) {
+		return tiles.get(coords);
+	}
+
+	public void addTileObserver(Observer observer) {
+		for (Map.Entry<Coordinates, Tile> entry : tiles.entrySet()) {
+			entry.getValue().addObserver(observer);
+		}
+	}
 }
--- src/data/Tile.java
+++ src/data/Tile.java
@@ -2,68 +2,164 @@ package data;
 
 import java.io.Serializable;
 import java.util.Observable;
+import java.util.Observer;
 
-//TODO
 public final class Tile extends Observable implements GameData {
 	/*
-	 *  -----------------------------------------------Attributes------------------------------------------------------------
+	 * -----------------------------------------------Attributes------------------
+	 * ------------------------------------------
 	 */
+	protected final Coordinates coordinates;
 	protected Piece piece;
-	
+	protected boolean reachable = false;
+	protected boolean selected = false;
+
 	/*
-	 *  -----------------------------------------------Methodes------------------------------------------------------------
+	 * -----------------------------------------------Methodes--------------------
+	 * ----------------------------------------
 	 */
 
+	public static enum TileModificationType {
+		INITIALISATION, TURN_RESET, FLAGS_CHANGED;
+	}
+
+	/**
+	 * Immutable, {@link Serializable} class that describes a modification that
+	 * occurred on a {@link Tile}. <br>
+	 * Used to notify observers of a modification in the state of an observed
+	 * {@link Tile}.
+	 */
 	public static final class TileModification implements Serializable {
-		private static final long serialVersionUID = 935932699665702904L;
-		//TODO
+		private static final long serialVersionUID = -974482670427287450L;
+		private final TileModificationType type;
+		private final boolean newReachableFlag;
+		private final boolean newSelectedFlag;
+
+		private TileModification(TileModificationType type,
+				boolean newReachableFlag, boolean newSelectedFlag) {
+			super();
+			this.type = type;
+			this.newReachableFlag = newReachableFlag;
+			this.newSelectedFlag = newSelectedFlag;
+		}
+
+		public TileModificationType getType() {
+			return type;
+		}
+
+		public boolean isNowReachable() {
+			return newReachableFlag;
+		}
+
+		public boolean isNowSelected() {
+			return newSelectedFlag;
+		}
+	}
+
+	public Tile(Coordinates coordinates) {
+		this.coordinates = coordinates;
+	}
+
+	@Override
+	public synchronized void addObserver(Observer o) {
+		super.addObserver(o);
+		o.update(this, new TileModification(
+				TileModificationType.INITIALISATION, this.reachable,
+				this.selected));
+	}
+
+	/**
+	 * Applies the modifications described by <code>updateDescription</code>
+	 * 
+	 * @param updateDescription
+	 *            The description of the modification
+	 */
+	public void update(TileModification updateDescription) {
+		notifyObservers(updateDescription);
+
+		switch (updateDescription.getType()) {
+		case FLAGS_CHANGED:
+		case INITIALISATION:
+		case TURN_RESET:
+			this.reachable = updateDescription.isNowReachable();
+			this.selected = updateDescription.isNowSelected();
+			break;
+		}
 	}
 
 	@Override
 	public void resetTurn() {
-		// TODO Auto-generated method stub
-		
+		notifyObservers(new TileModification(TileModificationType.TURN_RESET,
+				false, false));
+		selected = false;
+		reachable = false;
 	}
 
 	@Override
 	public void endTurn() {
-		// TODO Auto-generated method stub
-		
+		notifyObservers(new TileModification(
+				TileModificationType.FLAGS_CHANGED, false, false));
+		selected = false;
+		reachable = false;
 	}
-	
-	public Coordinates getCoordinates(){
-		// TODO
-		return null;
+
+	/**
+	 * @return The coordinates of this tile on the {@link Board}.
+	 */
+	public Coordinates getCoordinates() {
+		return coordinates;
 	}
-	
+
+	/**
+	 * @return The {@link Piece} currently on this tile, or <code>null</code> if
+	 *         there isn't any.
+	 */
 	public Piece getPiece() {
 		return piece;
 	}
-	
+
+	/**
+	 * @return If the currently selected {@link Piece} can reach this tile,
+	 *         returns <code>true</code>. If there is no currently selected
+	 *         {@link Piece} or if it cannot reach this tile, returns
+	 *         <code>false</code>
+	 */
 	public boolean isReachable() {
-		// TODO
-		return false;
+		return reachable;
 	}
-	
+
+	/**
+	 * @return If this tile is currently selected (is on the path of the
+	 *         selected {@link Piece}), returns <code>true</code>. If there is
+	 *         no currently selected {@link Piece} or if this tile is not
+	 *         selected, returns <code>false</code>
+	 */
 	public boolean isSelected() {
-		// TODO
-		return false;
+		return selected;
 	}
-	
+
+	/**
+	 * @return If there is currently a {@link Piece} on this tile, returns
+	 *         <code>true</code>. Otherwise, returns <code>false</code>
+	 */
 	public boolean isEmpty() {
-		//TODO: just return true if piece == null, false otherwise
-		return false;
+		return piece == null;
 	}
 
 	void setPiece(Piece newPiece) {
+		assert (piece != null);
 		piece = newPiece;
 	}
-	
+
 	void setSelected(boolean selected) {
-		//TODO
+		notifyObservers(new TileModification(
+				TileModificationType.FLAGS_CHANGED, this.reachable, selected));
+		this.selected = selected;
 	}
-	
-	void setReachable(boolean selected) {
-		//TODO
+
+	void setReachable(boolean reachable) {
+		notifyObservers(new TileModification(
+				TileModificationType.FLAGS_CHANGED, reachable, this.selected));
+		this.reachable = reachable;
 	}
 }
